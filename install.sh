d3bbef7a3eba31e836b43b9cffd1b173375b012b
WenbWa <wenbwang@student.ethz.ch>
2024-12-11 23:54:36 +0800

    final_release

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 # install UniGraspTransformer
 pip install -e .
+
 # install pytorch_kinematics
 cd pytorch_kinematics
 pip install -e .
 cd ..
+
 # install pytorch3d
 pip install "git+https://github.com/facebookresearch/pytorch3d.git"
 
 # prepare folders
 python prepare.py
-# unzip hand_assets
-cd dexgrasp
-unzip hand_assets.zip
-
